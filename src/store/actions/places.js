49edb951a8c80514bcdd40fe8dd846745871cf8f
Brad Tracy <btracy@everesttech.com>
2019-03-04 05:49:43 -0500

    images are uploading to google cloud storage

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,15 +1,27 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+// https://us-central1-fb-rnplay.cloudfunctions.net/storeImage
 
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
-        const placeData = {
-            name: placeName,
-            location: location
-        };
-        fetch("https://fb-rnplay.firebaseio.com/places.json", {
+        fetch("https://us-central1-fb-rnplay.cloudfunctions.net/storeImage", {
             method: "POST",
-            body: JSON.stringify(placeData)
+            body: JSON.stringify({
+                image: image.base64
+            })
         })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://fb-rnplay.firebaseio.com/places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })  
         .catch(err => console.log(err))
         .then(res => res.json())
         .then(parsedRes => {
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,19 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-
 import placesReducer from './reducers/places';
 
 const rootReducer = combineReducers({
     places: placesReducer
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-    return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configureStore;
